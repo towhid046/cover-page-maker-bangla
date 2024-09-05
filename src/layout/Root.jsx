1a42c830fb58f44c44aeca49b4b4094e3b5834bc
towhid046 <towhidmorol46@gmail.com>
2024-09-05 23:07:49 +0600

    rename TempComponents to components

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../TemComponents/shared/Footer/Footer.jsx";
-import Navbar from "../TemComponents/shared/Navbar/Navbar.jsx";
+import Footer from "../components/shared/Footer/Footer.jsx";
+import Navbar from "../components/shared/Navbar/Navbar.jsx";
 const Root = () => {
   return (
     <div>
