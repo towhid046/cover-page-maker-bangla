8cdd1ff60bc28ef3869a881a1886c065f2369e9c
towhid046 <towhidmorol46@gmail.com>
2024-09-06 17:00:52 +0600

    test

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/Routes";
-import './index.css'
-import { initialize } from 'react-ga4';
+import "./index.css";
+import ReactGA from "react-ga4";
 
-initialize('G-EFHK8GFYD5'); 
+ReactGA.initialize("G-EFHK8GFYD5");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
