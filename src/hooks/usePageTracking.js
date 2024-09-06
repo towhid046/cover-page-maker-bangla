431f906d61faf275eddcbaf864bad2f98208cb43
towhid046 <towhidmorol46@gmail.com>
2024-09-06 17:03:33 +0600

    test

--- src/hooks/usePageTracking.js
+++ src/hooks/usePageTracking.js
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { trackPageview } from 'react-ga4';
+import ReactGA from 'react-ga4';
 
 const usePageTracking = () => {
   const location = useLocation();
 
   useEffect(() => {
-    trackPageview(location.pathname + location.search);
+    ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
   }, [location]);
 };
 
