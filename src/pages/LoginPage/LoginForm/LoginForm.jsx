4b0c7895f9483c752550b9952e0c749b5c8592ea
towhid046 <towhidmorol46@gmail.com>
2024-09-07 11:41:19 +0600

    design login and register page

---
+++ src/pages/LoginPage/LoginForm/LoginForm.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import Button from "../../../components/shared/Button/Button";
+
+const commonInputClassName =
+  "py-2 px-3 border border-blue-400 rounded-md focus:outline-none transition duration-300";
+const inputParentClassName = "flex flex-col gap-1 text-lg mb-3 ";
+
+const LoginForm = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  // Handle form submission
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* Email */}
+      <div className={inputParentClassName}>
+        <label>Email:</label>
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^\S+@\S+\.\S+$/,
+              message: "Entered value does not match email format",
+            },
+          })}
+          type="email"
+          placeholder="Your Email"
+          className={commonInputClassName}
+          required
+        />
+        {errors?.email && (
+          <span className="text-red-500">{errors?.email.message}</span>
+        )}
+      </div>
+
+      {/* Name */}
+      <div className={inputParentClassName}>
+        <label>Password:</label>
+        <input
+          {...register("password")}
+          required
+          type="password"
+          placeholder="Password"
+          className={commonInputClassName}
+        />
+      </div>
+
+      {/* Submit Button */}
+      <div>
+        <Button customClass="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300">
+          Login
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
-import useScrollToTop from '../../hooks/useScrollToTop'
+import { Link } from "react-router-dom";
+import useScrollToTop from "../../hooks/useScrollToTop";
+import RegisterForm from "./RegisterForm/RegisterForm";
+import SocialLogin from "./../LoginPage/SocialLogin/SocialLogin";
 
 const RegistrationPage = () => {
-  useScrollToTop()
+  useScrollToTop();
 
   return (
-    <div>RegistrationPage</div>
-  )
-}
+    <section className="flex items-center container mx-auto justify-center min-h-screen text-gray-700 px-4">
+      <div className="flex rounded-xl bg-gray-100 flex-col lg:flex-row lg:gap-12 gap-6 lg:py-20 py-12 lg:px-16 px-6 ">
+        <div className="flex-1 flex  flex-col lg:space-y-7 space-y-3">
+          <div className="">
+            <Link
+              to="/"
+              className="font-titillium md:text-5xl text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+            >
+              CPG
+            </Link>
+            <h2 className="text-gray-700 lg:text-4xl text-3xl font-titillium font-bold">
+              Register Now!
+            </h2>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus veniam iure rem ipsum dolorum earum rerum tenetur
+            pariatur eum eos totam, neque libero? Nobis molestias eum quam
+            soluta nesciunt unde?
+          </p>
+          <p>
+            Already Register ?{" "}
+            <Link to={"/login"} className="text-blue-500">
+              Login
+            </Link>
+          </p>
+        </div>
 
-export default RegistrationPage
+        <div className="flex-1">
+          <div className="w-full  rounded-lg">
+            <RegisterForm />
+            <SocialLogin />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RegistrationPage;
